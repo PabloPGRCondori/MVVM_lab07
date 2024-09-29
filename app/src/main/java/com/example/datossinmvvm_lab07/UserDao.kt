c399afe41c6c100090576c9e2b5489fa24707209
PabloPGRCondori <tsukuraishimotlo@gmail.com>
2024-09-29 14:54:09 -0500

    Nueva interfaz creada, con el nombre de UserDao

---
+++ app/src/main/java/com/example/datossinmvvm_lab07/UserDao.kt
@@ -0,0 +1,14 @@
+package com.example.datossinmvvm_lab07
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM User")
+    suspend fun getAll(): List<User>
+
+    @Insert
+    suspend fun insert(user: User)
+}
